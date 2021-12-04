c8343d0f6644fb3898361f0116a68c072a4b57ef
邴新科 <kylebing@163.com>
2021-12-04 11:32:05 +0800

    rect 大小，偏移量

--- canvas.js
+++ canvas.js
@@ -12,28 +12,28 @@ window.onload = () => {
     setFrame()
     let canvas = getCanvas().getContext('2d')
     let direction = 1 // 变化方向
-    let root = 0 // 变化值 初始值
+    let seed = 0 // 变化值 初始值
     const strike = 1 // 每帧间隔
     setInterval(()=>{
-        // render(canvas, root)
-        renderRect(canvas, root)
-        root = root + (strike * direction)
-        if (root < 0){
+        // renderCircle(canvas, seed)
+        renderRect(canvas, seed)
+        seed = seed + (strike * direction)
+        if (seed < 0){
             direction = 1
         }
-        if (root > 15 ){
+        if (seed > 25 ){
             direction = -1
         }
-    }, 1000/5)
+    }, 1000/30)
 }
 
-function render(canvas, root){
+function renderCircle(canvas, seed){
     canvas.closePath()
     for (let i=0;i<15;i++){
         for(let j=0; j<25; j++){
             canvas.beginPath()
-            canvas.arc(base.x + grid + ball.x + j * ball.radius * 3, base.y + grid * i + ball.y, ball.radius, 0,Math.PI * 2 / 250 * root, true)
-            canvas.fillStyle = `rgba(${i * i * 0.3 * i + root},${j * j*  2 + root},${10 * j}, 1)`
+            canvas.arc(base.x + grid + ball.x + j * ball.radius * 3, base.y + grid * i + ball.y, ball.radius, 0,Math.PI * 2 / 250 * seed, true)
+            canvas.fillStyle = `rgba(${i * i * 0.3 * i + seed},${j * j*  2 + seed},${10 * j}, 1)`
             canvas.fill()
             // canvas.strokeStyle = `rgba(${i * i * 0.3 * i + root},${j * j*  2 + root},${10 * j}, 1)`
             // canvas.stroke()
@@ -42,20 +42,22 @@ function render(canvas, root){
     }
 }
 
-function renderRect(canvas, root){
+function renderRect(canvas, seed){
     console.clear()
-    const offsetPosition = 80 // 位置偏移量
-    const offsetColor = 60 // 颜色偏移量
+    const offsetPosition = 100 // 位置偏移量
+    const offsetColor = 15 // 颜色偏移量
+    const rectSize = [200, 200] // rect Size
+    const countRect = 20 // rect 数量
     clearFrame(canvas)
-    for(let i=0; i<4; i++){
-        console.log(root * 7 + offsetColor * i, 100 + i * offsetPosition)
-        canvas.fillRect(100 + i * offsetPosition, 100 + i * offsetPosition, 200,200)
+    for(let i=0; i<20; i++){
+        console.log(seed * 7 + offsetColor * i, 100 + i * offsetPosition)
+        canvas.fillRect(100 + i * offsetPosition, 100 + i * offsetPosition, rectSize[0],rectSize[1])
         canvas.fillStyle = `rgba(
-            ${root * 7 + offsetColor * i }, 
-            ${root * 7 + offsetColor * i }, 
-            ${root * 7 + offsetColor * i }, 1)`
+            ${seed * 10 + offsetColor * i }, 
+            ${seed * 10 + offsetColor * i }, 
+            ${seed * 10 + offsetColor * i }, 1)`
         canvas.fill()
-        canvas.strokeRect(100 + i * offsetPosition, 100 + i * offsetPosition, 200,200)
+        canvas.strokeRect(100 + i * offsetPosition, 100 + i * offsetPosition, rectSize[0],rectSize[1])
         canvas.strokeStyle = 'rgba(0,0,0,0.5)'
         canvas.stroke()
     }
